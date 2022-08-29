2439940397bffceae01a59c5329a66b27f8f3351
Nayeem Hasan <hasannayeeem@gmail.com>
2022-08-30 02:27:49 +0600

    created some model and schema

--- server/models/chatModel.js
+++ server/models/chatModel.js
@@ -1,2 +1,33 @@
+const mongoose = require('mongoose');
+
+const chatModel = mongoose.Schema({
+    chatName:{type:String,trim:true},
+    isGroupChat: {type: String,default: false},
+    users:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    latestMessage:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Message", 
+    },
+    groutAdmin:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }
+},
+{
+    timestamps:true,
+}
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.exports = Chat;
+
+
 // chatName
 // isGroupChat
+// users
+// latestMessage
+// groupAdmin
